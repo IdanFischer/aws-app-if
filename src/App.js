32784d814e7e91bf6c769d240c37959e7ec6b94e
Idan Fischer <Idanfischer5@gmail.com>
2023-02-23 16:05:38 -0500

    made a small react app with aws

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_API_ENDPOINT)
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_ENDPOINT)
+    .then(res => res.json())
+    .then(data => console.log('data from server -->', data))
+    .catch(console.error)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hi class</h1>
       </header>
     </div>
   );
